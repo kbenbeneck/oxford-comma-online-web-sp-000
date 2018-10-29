94b8bf170047cb8f5ee5edfb7d3cef4972d6e0d0
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-29 01:51:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.count == 1
     return array.join
   elsif array.count == 2
-    return array.join(" and ")
+    array[-2] << " and "
+    array.join
   elsif array.count > 2
     return array[-1].prepend "and "
     array.join(", ")
